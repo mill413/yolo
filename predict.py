8ec1411b1095d6e3157759c77d9f7fff1d682b58
mill <mill413@outlook.com>
2023-12-01 18:39:49 +0800

    update

--- predict.py
+++ predict.py
@@ -1,9 +1,20 @@
 from ultralytics import YOLO
 from utils import predict, config
+import argparse, os
 
-for model_name in config["models"]:
-    name = model_name.removesuffix(".pt") if model_name.endswith(".pt") else model_name.removesuffix(".yaml")
-    predict(
-        model = YOLO(f"./{config['project']}/{name}-train/weights/best.pt"),
-        source = "../datasets/VisDrone/VisDrone2019-DET-test-dev/images/",
-        project = config["project"], name = name + "-predict")
+parser = argparse.ArgumentParser(description="Predict model parse arguments.")
+
+parser.add_argument("--model", type=str, dest="model",
+                    help="trained pt model file in models/ dir", default="yolov8s.pt")
+parser.add_argument("--source", type=str, dest="source",
+                    help="predict source path", default="../datasets/VisDrone/VisDrone2019-DET-test-dev/images/")
+
+args = parser.parse_args()
+
+model_name = args.model
+name = model_name.removesuffix(".pt") if model_name.endswith(".pt") else model_name.removesuffix(".yaml")
+
+predict(
+    model=YOLO(f"./{name}/train/weights/best.pt"), source=args.source,
+    project=name, name="predict")
+    
--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
-python train.py
-python predict.py
+#!/bin/zsh
+models=("yolov8s" "yolov8-sod" "yolov5s")
+
+for model in ${models[*]}
+do
+    python train.py --model $model --epochs 200
+    python predict.py --model $model 
+done
