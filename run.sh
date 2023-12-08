6b26990083e21d7c29d935c28228e7e9c3ffe324
mill413 <mill413@outlook.com>
2023-12-08 16:31:47 +0800

    update shell script after sytax checking

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 models=(
 # "yolov8m-cbam"
 # "yolov8m"
@@ -12,19 +12,19 @@ dataset="VisDrone"
 test_source="/home/wsy_2022301480/jupyterlab/datasets/FLIR/test/data/video-4FRnNpmSmwktFJKjg-frame-000745-L6K5SC6fYjHNC8uff.jpg"
 log_file="./logs/$(date +%Y-%m-%d).log"
 
-if [ ! -f ${log_file} ];then
-    touch ${log_file}
+if [ ! -f "${log_file}" ];then
+    touch "${log_file}"
 fi
 
-echo "\nProgram Start!" >> ${log_file}
+printf "\nProgram Start!" >> "${log_file}"
 
-for model in ${models[*]};do
-    if [ $# -eq 1 ] && [ $1=="--test" ];then
-        python train.py --model $model --dataset $dataset --epochs 1 
-        python value.py --model $model --dataset $dataset
-       	python predict.py --model $model --source $test_source
+for model in "${models[@]}";do
+    if [ $# -eq 1 ] && [ "$1" == "--test" ];then
+        python train.py --model "$model" --dataset $dataset --epochs 1 
+        python value.py --model "$model" --dataset $dataset
+       	python predict.py --model "$model" --source $test_source
     elif [ $# -eq 0 ];then
-        python train.py --model $model --dataset $dataset --epochs 200 --workers 8 --batch 8
-        python value.py --model $model --dataset $dataset
+        python train.py --model "$model" --dataset $dataset --epochs 200 --workers 8 --batch 16
+        python value.py --model "$model" --dataset $dataset
     fi 
 done
