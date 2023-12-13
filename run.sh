2e24f99f0602cd3edc0096cf9ee50c4df122d975
mill413 <mill413@outlook.com>
2023-12-13 23:08:47 +0800

    update gitignore

--- run.sh
+++ run.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 models=(
-"yolov8m-cbam"
-# "yolov8m"
-# "yolov8m-p2"
 # "yolov5m"
-# "yolov5m-resnet"
-# "yolov5m-p2"
-"yolov5m-p234"
-"yolov5m-cbam"
+# "yolov5s-p234"
+# "yolov5m-cbam"
+# "yolov5s-cbam-p234"
+"yolov8s-acmix"
 )
 
 dataset="VisDrone"
-test_source="/home/wsy_2022301480/jupyterlab/datasets/VisDrone/VisDrone2019-DET-test-dev/images/0000006_00159_d_0000001.jpg"
+test_source="/home/haruto/Document/datasets/VisDrone/VisDrone2019-DET-test-dev/images/0000006_00159_d_0000001.jpg"
 log_file="./logs/$(date +%Y-%m-%d).log"
 
 if [ ! -f "${log_file}" ];then
@@ -22,11 +19,11 @@ printf "\nProgram Start!" >> "${log_file}"
 
 for model in "${models[@]}";do
     if [ $# -eq 1 ] && [ "$1" == "--test" ];then
-        python train.py --model "$model" --dataset $dataset --epochs 1 
+        python train.py --model "$model" --dataset $dataset --epochs 1 --workers 4 --batch 2
         python value.py --model "$model" --dataset $dataset
-       	python predict.py --model "$model" --source $test_source
+       	# python predict.py --model "$model" --source $test_source
     elif [ $# -eq 0 ];then
-        python train.py --model "$model" --dataset $dataset --epochs 200 --workers 8 --batch 16
+        python train.py --model "$model" --dataset $dataset --epochs 200 --workers 8 --batch 8
         python value.py --model "$model" --dataset $dataset
     fi 
 done
