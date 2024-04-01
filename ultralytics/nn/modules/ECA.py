5e5ea47516f5c772bdade550d3d95244adb85333
MillHaruto <mill413@outlook.com>
2024-04-01 18:45:00 +0800

    update eca

--- ultralytics/nn/modules/ECA.py
+++ ultralytics/nn/modules/ECA.py
@@ -2,7 +2,6 @@ import math
 from torch import nn
 
 
-# Efficient Channel Attention module
 class ECA(nn.Module):
     def __init__(self, c, b=1, gamma=2):
         super(ECA, self).__init__()
@@ -10,13 +9,13 @@ class ECA(nn.Module):
         k = t if t % 2 else t + 1
 
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.conv1 = nn.Conv1d(1, 1, kernel_size=k,
-                               padding=int(k/2), bias=False)
+        self.conv1 = nn.Conv1d(1, 1, kernel_size=k, padding=int(k/2), bias=False)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         out = self.avg_pool(x)
-        out = self.conv1(out.squeeze(-1).transpose(-1, -2)
-                         ).transpose(-1, -2).unsqueeze(-1)
+        out = self.conv1(out.squeeze(-1).transpose(-1, -2)).transpose(-1, -2).unsqueeze(-1)
         out = self.sigmoid(out)
         return out * x
+
+
