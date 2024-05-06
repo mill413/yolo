b9e5fce59c9d47622cff7b3a35c72318a5d03474
mill413 <mill413@outlook.com>
2024-05-06 19:06:39 +0800

    format code

--- heatmap.py
+++ heatmap.py
@@ -1,16 +1,21 @@
-import warnings
-warnings.filterwarnings('ignore')
-warnings.simplefilter('ignore')
-import torch, cv2, os, shutil
+import argparse
+import os
+import shutil
+
+import cv2
 import numpy as np
-np.random.seed(0)
-from tqdm import trange
+import torch
 from PIL import Image
-from ultralytics.nn.tasks import attempt_load_weights
-from ultralytics.utils.ops import xywh2xyxy, non_max_suppression
-from pytorch_grad_cam.utils.image import show_cam_on_image, scale_cam_image
-from pytorch_grad_cam.activations_and_gradients import ActivationsAndGradients
 from pytorch_grad_cam import GradCAM, HiResCAM
+from pytorch_grad_cam.activations_and_gradients import ActivationsAndGradients
+from pytorch_grad_cam.utils.image import scale_cam_image, show_cam_on_image
+from tqdm import trange
+
+from ultralytics.nn.tasks import attempt_load_weights
+from ultralytics.utils.ops import non_max_suppression, xywh2xyxy
+
+np.random.seed(0)
+
 
 def letterbox(im, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True, stride=32):
     # Resize and pad image while meeting stride-multiple constraints
@@ -26,13 +31,15 @@ def letterbox(im, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleF
     # Compute padding
     ratio = r, r  # width, height ratios
     new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
-    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
+    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - \
+        new_unpad[1]  # wh padding
     if auto:  # minimum rectangle
         dw, dh = np.mod(dw, stride), np.mod(dh, stride)  # wh padding
     elif scaleFill:  # stretch
         dw, dh = 0.0, 0.0
         new_unpad = (new_shape[1], new_shape[0])
-        ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]  # width, height ratios
+        ratio = new_shape[1] / shape[1], new_shape[0] / \
+            shape[0]  # width, height ratios
 
     dw /= 2  # divide padding into 2 sides
     dh /= 2
@@ -41,8 +48,10 @@ def letterbox(im, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleF
         im = cv2.resize(im, new_unpad, interpolation=cv2.INTER_LINEAR)
     top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
     left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
-    im = cv2.copyMakeBorder(im, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
+    im = cv2.copyMakeBorder(im, top, bottom, left, right,
+                            cv2.BORDER_CONSTANT, value=color)  # add border
     return im, ratio, (dw, dh)
+
 
 class ActivationsAndGradients:
     """ Class for extracting activations and
@@ -87,17 +96,19 @@ class ActivationsAndGradients:
         boxes_ = result[:, :4]
         sorted, indices = torch.sort(logits_.max(1)[0], descending=True)
         return torch.transpose(logits_[0], dim0=0, dim1=1)[indices[0]], torch.transpose(boxes_[0], dim0=0, dim1=1)[indices[0]], xywh2xyxy(torch.transpose(boxes_[0], dim0=0, dim1=1)[indices[0]]).cpu().detach().numpy()
-  
+
     def __call__(self, x):
         self.gradients = []
         self.activations = []
         model_output = self.model(x)
-        post_result, pre_post_boxes, post_boxes = self.post_process(model_output[0])
+        post_result, pre_post_boxes, post_boxes = self.post_process(
+            model_output[0])
         return [[post_result, pre_post_boxes]]
 
     def release(self):
         for handle in self.handles:
             handle.remove()
+
 
 class yolov8_target(torch.nn.Module):
     def __init__(self, ouput_type, conf, ratio) -> None:
@@ -105,7 +116,7 @@ class yolov8_target(torch.nn.Module):
         self.ouput_type = ouput_type
         self.conf = conf
         self.ratio = ratio
-    
+
     def forward(self, data):
         post_result, pre_post_boxes = data
         result = []
@@ -119,6 +130,7 @@ class yolov8_target(torch.nn.Module):
                     result.append(pre_post_boxes[i, j])
         return sum(result)
 
+
 class yolov8_heatmap:
     def __init__(self, weight, device, method, layer, backward_type, conf_threshold, ratio, show_box, renormalize):
         device = torch.device(device)
@@ -129,22 +141,27 @@ class yolov8_heatmap:
         for p in model.parameters():
             p.requires_grad_(True)
         model.eval()
-        
+
         target = yolov8_target(backward_type, conf_threshold, ratio)
         target_layers = [model.model[l] for l in layer]
-        method = eval(method)(model, target_layers, use_cuda=device.type == 'cuda')
-        method.activations_and_grads = ActivationsAndGradients(model, target_layers, None)
-        
-        colors = np.random.uniform(0, 255, size=(len(model_names), 3)).astype(int)
+        method = eval(method)(model, target_layers,
+                              use_cuda=device.type == 'cuda')
+        method.activations_and_grads = ActivationsAndGradients(
+            model, target_layers, None)
+
+        colors = np.random.uniform(
+            0, 255, size=(len(model_names), 3)).astype(int)
         self.__dict__.update(locals())
-    
+
     def post_process(self, result):
-        result = non_max_suppression(result, conf_thres=self.conf_threshold, iou_thres=0.65)[0]
+        result = non_max_suppression(
+            result, conf_thres=self.conf_threshold, iou_thres=0.65)[0]
         return result
 
     def draw_detections(self, box, color, name, img):
         xmin, ymin, xmax, ymax = list(map(int, list(box)))
-        cv2.rectangle(img, (xmin, ymin), (xmax, ymax), tuple(int(x) for x in color), 2)
+        cv2.rectangle(img, (xmin, ymin), (xmax, ymax),
+                      tuple(int(x) for x in color), 2)
         # cv2.putText(img, str(name), (xmin, ymin - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.8, tuple(int(x) for x in color), 2, lineType=cv2.LINE_AA)
         return img
 
@@ -154,40 +171,46 @@ class yolov8_heatmap:
         renormalized_cam = np.zeros(grayscale_cam.shape, dtype=np.float32)
         for x1, y1, x2, y2 in boxes:
             x1, y1 = max(x1, 0), max(y1, 0)
-            x2, y2 = min(grayscale_cam.shape[1] - 1, x2), min(grayscale_cam.shape[0] - 1, y2)
-            renormalized_cam[y1:y2, x1:x2] = scale_cam_image(grayscale_cam[y1:y2, x1:x2].copy())    
+            x2, y2 = min(grayscale_cam.shape[1] - 1,
+                         x2), min(grayscale_cam.shape[0] - 1, y2)
+            renormalized_cam[y1:y2, x1:x2] = scale_cam_image(
+                grayscale_cam[y1:y2, x1:x2].copy())
         renormalized_cam = scale_cam_image(renormalized_cam)
-        eigencam_image_renormalized = show_cam_on_image(image_float_np, renormalized_cam, use_rgb=True)
+        eigencam_image_renormalized = show_cam_on_image(
+            image_float_np, renormalized_cam, use_rgb=True)
         return eigencam_image_renormalized
-    
+
     def process(self, img_path, save_path):
         # img process
         img = cv2.imread(img_path)
         img = letterbox(img)[0]
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         img = np.float32(img) / 255.0
-        tensor = torch.from_numpy(np.transpose(img, axes=[2, 0, 1])).unsqueeze(0).to(self.device)
-        
+        tensor = torch.from_numpy(np.transpose(
+            img, axes=[2, 0, 1])).unsqueeze(0).to(self.device)
+
         try:
             grayscale_cam = self.method(tensor, [self.target])
         except AttributeError as e:
             return
-        
+
         grayscale_cam = grayscale_cam[0, :]
         cam_image = show_cam_on_image(img, grayscale_cam, use_rgb=True)
-        
+
         pred = self.model(tensor)[0]
         pred = self.post_process(pred)
         if self.renormalize:
-            cam_image = self.renormalize_cam_in_bounding_boxes(pred[:, :4].cpu().detach().numpy().astype(np.int32), img, grayscale_cam)
+            cam_image = self.renormalize_cam_in_bounding_boxes(
+                pred[:, :4].cpu().detach().numpy().astype(np.int32), img, grayscale_cam)
         if self.show_box:
             for data in pred:
                 data = data.cpu().detach().numpy()
-                cam_image = self.draw_detections(data[:4], self.colors[int(data[4:].argmax())], f'{self.model_names[int(data[4:].argmax())]} {float(data[4:].max()):.2f}', cam_image)
-        
+                cam_image = self.draw_detections(data[:4], self.colors[int(data[4:].argmax(
+                ))], f'{self.model_names[int(data[4:].argmax())]} {float(data[4:].max()):.2f}', cam_image)
+
         cam_image = Image.fromarray(cam_image)
         cam_image.save(save_path)
-    
+
     def __call__(self, img_path, save_path):
         # remove dir if exist
         if os.path.exists(save_path):
@@ -197,27 +220,43 @@ class yolov8_heatmap:
 
         if os.path.isdir(img_path):
             for img_path_ in os.listdir(img_path):
-                self.process(f'{img_path}/{img_path_}', f'{save_path}/{img_path_}')
+                self.process(f'{img_path}/{img_path_}',
+                             f'{save_path}/{img_path_}')
         else:
             self.process(img_path, f'{save_path}/result.png')
 
 
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Heatmap parse arguments.")
+
+    parser.add_argument("--model", type=str, dest="model",
+                        help="trained pt model file in models/ dir")
+    parser.add_argument("--source", type=str, dest="source",
+                    help="predict source path", default="../datasets/VisDrone/VisDrone2019-DET-test-dev/images/")
+    parser.add_argument("--dataset", type=str, dest="dataset",
+                        help="predict dataset", default="visdrone")
+    args = parser.parse_args()
+
+    model_name = args.model
+    source = args.source
+    dataset = args.dataset
+
+    return (model_name, source, dataset)
+
+
 if __name__ == '__main__':
-    model_name = "baseline/yolov8s"
-    dataset = "flir"
+    model_name, source, dataset = parse_arguments()
     params = {
-        # 现在只需要指定权重即可,不需要指定cfg
-        'weight': 'runs1/runs/baseline/yolov8s/flir/train/weights/best.pt',
-        'device': 'cuda:0',
-        # GradCAMPlusPlus, GradCAM, XGradCAM, EigenCAM, HiResCAM, LayerCAM, RandomCAM, EigenGradCAM
-        'method': 'HiResCAM',
-        'layer': [15, 18, 21],
-        'backward_type': 'all',  # class, box, all
-        'conf_threshold': 0.2,  # 0.2
-        'ratio': 0.02,  # 0.02-0.1
-        'show_box': False,
-        'renormalize': False
+        "weight": f"runs/{model_name}/{dataset}/train/weights/best.pt",
+        "device": "cuda:0",
+        "method": "HiResCAM", # GradCAMPlusPlus, GradCAM, XGradCAM, EigenCAM, HiResCAM, LayerCAM, RandomCAM, EigenGradCAM
+        "layer": [15, 18, 21],
+        "backward_type": "all",  # class, box, all
+        "conf_threshold": 0.2,  # 0.2
+        "ratio": 0.02,  # 0.02-0.1
+        "show_box": False,
+        "renormalize": False
     }
     model = yolov8_heatmap(**params)
-    model(r"/home/wsy_2022301480/jupyterlab/datasets/FLIR/test/data/v3",
+    model(source,
           f"runs/{model_name}/{dataset}/heatmap")
