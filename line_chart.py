8a7401e295853b5adec831c9603ed7653f49e192
mill413 <mill413@outlook.com>
2024-05-06 23:11:21 +0800

    add line_chart

---
+++ line_chart.py
@@ -0,0 +1,114 @@
+import csv
+import os
+from dataclasses import dataclass
+
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import MultipleLocator
+import matplotlib.colors
+import random
+
+def remove_spaces(file: str):
+    with open(file, 'r') as f:
+        lines = f.readlines()
+    with open(file+".tmp", 'w') as f:
+        for line in lines:
+            f.write(line.replace(' ', ''))
+
+
+def read_from_csv(csvFile: str):
+    remove_spaces(csvFile)
+    data = {
+        "box_loss": [],
+        "cls_loss": [],
+        "dfl_loss": [],
+        "precision": [],
+        "recall": [],
+        "mAP50": [],
+        "mAP50-95": []
+    }
+
+    with open(csvFile+".tmp", 'r') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            data["box_loss"].append(float(row["train/box_loss"]))
+            data["cls_loss"].append(float(row["train/cls_loss"]))
+            data["dfl_loss"].append(float(row["train/dfl_loss"]))
+            data["precision"].append(float(row["metrics/precision(B)"]))
+            data["recall"].append(float(row["metrics/recall(B)"]))
+            data["mAP50"].append(float(row["metrics/mAP50(B)"]))
+            data["mAP50-95"].append(float(row["metrics/mAP50-95(B)"]))
+    os.remove(csvFile+".tmp")
+    return data
+
+
+def draw_line(dataFile: str, dataName: str, lineColor: str, key: str):
+    data = read_from_csv(dataFile)
+    plt.plot(
+        range(1, 201), data[key],
+        color=lineColor, alpha=1,
+        linestyle='-', linewidth=1,
+        marker='.', markevery=range(1, 201, 10),
+        label=dataName
+    )
+
+
+@dataclass
+class DataLine:
+    file: str
+    name: str
+    color: str
+
+
+def generate_pic(
+        x_label: str, y_label: str,
+        key: str,
+        lines: list[DataLine],
+        save_file: str = "res.png",
+        show=False
+):
+
+    plt.figure(figsize=(10, 6))
+
+    for line in lines:
+        draw_line(
+            line.file,
+            line.name,
+            line.color,
+            key
+        )
+
+    ax = plt.gca()
+    ax.xaxis.set_major_locator(MultipleLocator(10))
+
+    plt.legend(fontsize=20)
+    plt.xlabel(x_label, fontsize=20)
+    plt.xticks(fontsize=10)
+    plt.ylabel(y_label, fontsize=20)
+    plt.yticks(fontsize=10)
+
+    if show:
+        plt.show()
+
+    plt.savefig("runs/"+save_file)
+
+lines = []
+colors = ["red", "lightgreen", "blue", "deepskyblue", "violet"]
+random.shuffle(colors)
+for ind, model in enumerate(["baseline", "p2", "p234", "nwd"]):
+    lines.append(DataLine(
+        f"runs/{model}/yolov8s/visdrone/train/results.csv",
+        model.capitalize(),
+        colors[ind]))
+
+generate_pic("Epoch", "Precision", "precision",
+             lines,
+             "precision.png")
+generate_pic("Epoch", "Recall", "recall",
+             lines,
+             "recall.png")
+generate_pic("Epoch", "mAP@50", "mAP50",
+             lines,
+             "mAP50.png")
+generate_pic("Epoch", "mAP@50:95", "mAP50-95",
+             lines,
+             "mAP50-95.png")
