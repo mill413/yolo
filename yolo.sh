41abf2d6ce66c6a5fa68d2683654480004f88728
mill413 <mill413@outlook.com>
2024-05-31 16:15:16 +0800

    reorganize files

--- yolo.sh
+++ yolo.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 # ./scripts/run.sh -p baseline --v5 s --v8 s -d visdrone -e 200 --predict | tee last.log
+
+./scripts/run.sh -p siou --v5 s --v8 s -d auav --predict | tee last.log &&
+./scripts/run.sh -p p2 --v5 s --v8 s -d auav --predict | tee last.log
